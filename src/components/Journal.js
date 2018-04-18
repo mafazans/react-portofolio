6145f6369b8445cdf58146b0128693e3b1c45df6
mafazans <arif.mafazan@gmail.com>
2018-04-18 10:36:18 +0700

    add state to blog posts

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Journal extends Component {
-	 data = [
-	  {id:1, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful3'},
-	  {id:2, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful1'},
-	  {id:3, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful2'},
-	  {id:4, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful4'},
-	  {id:5, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful5'},
-	  {id:6, title: 'THE THING ABOUT ES6', body: 'all about you is beautiful6'},
-	]
+	constructor(){
+		super();
+		this.state = {
+			posts: []
+		};
+	}
+
+componentDidMount() {
+  axios
+    .get(`https://mafazans.herokuapp.com/api`)
+    .then(res => this.setState({ posts: res.data }))
+    .catch(err => console.log(err))
+}
+
 	render() {
+		// console.log(this.state.posts);
 		return(
 			<div id="journal" className="text-left paddsection">
 			  <div className="container">
@@ -21,14 +29,14 @@ class Journal extends Component {
 			    <div className="journal-block">
 			      <div className="row">
 			      {
-			      	this.data.map(( {id, title, body} ) => {
+			      	this.state.posts.map(( {_id, title, content} ) => {
 								return(
-									<div key={id} className="col-lg-4 col-md-6">
+									<div key={_id} className="col-lg-4 col-md-6">
 									  <div className="journal-info">
 									    <a href="blog-single.html"><img src="images/blog-post-1.jpg" className="img-responsive" alt="img" /></a>
 									    <div className="journal-txt">
 									      <h4><a href="blog-single.html">{title}</a></h4>
-									      <p className="separator">{body}</p>
+									      <p className="separator">{content}</p>
 									    </div>
 									  </div>
 									</div>
